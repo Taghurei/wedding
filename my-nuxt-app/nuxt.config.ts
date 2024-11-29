ad6dcb77d81a10d7a271562a8fe0989c77dec0d2
Gauthier <gauthier@getunlatch.com>
2024-11-29 17:34:51 +0100

    core(deploy): add dist folder to deploy to github action

--- my-nuxt-app/nuxt.config.ts
+++ my-nuxt-app/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: '/wedding/', // Replace with your repository name
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
